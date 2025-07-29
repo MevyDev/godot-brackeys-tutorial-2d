92063afc75f92746bd082e859be0ce4608b8a71d
MevyDev <mevydev@proton.me>
2025-07-29 16:02:56 +0200

    export player stat variables

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED: int = 100
+@export var JUMP_VELOCITY: int = -200
 
 
 func _physics_process(delta: float) -> void:
