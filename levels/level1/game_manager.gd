eb68c8b33ae49b1c393dffcb201f45361324f350
MevyDev <mevydev@proton.me>
2025-07-31 08:14:23 +0200

    score label

--- levels/level1/game_manager.gd
+++ levels/level1/game_manager.gd
@@ -1,11 +1,10 @@
-extends Node2D
+extends Node
 
+@onready var coin_text: Label = $CoinText
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var score = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func add_point():
+	score += 1
+	coin_text.text = "You collected " + str(score) + " coins!"
+	print(score)
