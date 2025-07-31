dc0d48b0c8d4b4bdc43d6ee99cdfbfc7d211f0dd
MevyDev <mevydev@proton.me>
2025-07-31 07:27:42 +0200

    enemy flip sprite on direction change

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -6,10 +6,13 @@ var direction = 1
 
 @onready var ray_cast_left = $RayCastLeft
 @onready var ray_cast_right = $RayCastRight
+@onready var animatable_body = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	if(ray_cast_left.is_colliding()):
 		direction = 1
+		animatable_body.flip_h = false
 	if(ray_cast_right.is_colliding()):
 		direction = -1
+		animatable_body.flip_h = true
 	position.x += speed * delta * direction
